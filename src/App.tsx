f5091c6089f09fc4c9adaa21bbe26759ee578b3d
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-08-08 18:46:18 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import AgeGateWrapper from './components/AgeGateWrapper';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import VideoPage from "./pages/VideoPage";
+import CategoryPage from "./pages/CategoryPage";
+import PremiumPage from "./pages/PremiumPage";
+import AdminPanel from "./pages/AdminPanel";
+import ContactUs from "./pages/ContactUs";
+import TermsOfService from "./pages/TermsOfService";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import AuthPage from "./pages/AuthPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <AgeGateWrapper>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/video/:id" element={<VideoPage />} />
-            <Route path="/category/:category" element={<CategoryPage />} />
-            <Route path="/premium" element={<PremiumPage />} />
-            <Route path="/auth" element={<AuthPage />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/terms" element={<TermsOfService />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="/admin-hubx-2024" element={<AdminPanel />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </AgeGateWrapper>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/video/:id" element={<VideoPage />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
+          <Route path="/premium" element={<PremiumPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/admin-hubx-2024" element={<AdminPanel />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
   </QueryClientProvider>
 );
+
+export default App;
