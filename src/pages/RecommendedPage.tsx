ef7a3309846cd825c910f9a172cb104d956014d2
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-08-09 14:34:08 +0100

    Update RecommendedPage.tsx

--- src/pages/RecommendedPage.tsx
+++ src/pages/RecommendedPage.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import Recommended from '../components/Recommended';
 
-const RecommendedPage = () => {
+import React from 'react';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Recommended from '@/components/Recommended';
+
+const RecommendedPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="container mx-auto px-4 py-8">
+      <Header />
+      <main className="container mx-auto px-4 py-8">
         <Recommended />
-      </div>
+      </main>
+      <Footer />
     </div>
   );
 };
