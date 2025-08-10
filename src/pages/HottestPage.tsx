624ec45c3130baf0fd790485be6e587238a06334
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-08-10 11:04:32 +0100

    Update HottestPage.tsx

--- src/pages/HottestPage.tsx
+++ src/pages/HottestPage.tsx
@@ -78,14 +78,6 @@ const HottestPage = () => {
               <AdComponent zoneId="5660536" />
             </div>
           </div>
-          
-          {/* Secondary Ad Row */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="flex justify-center">
-              <AdComponent zoneId="5661270" />
-            </div>
-    
-
         {/* Videos Grid */}
         {loading ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
