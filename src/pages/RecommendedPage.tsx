e7b4bb674b0f0a8c690d69bdaa25f719bc51fc45
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-08-10 15:52:16 +0100

    Update RecommendedPage.tsx

--- src/pages/RecommendedPage.tsx
+++ src/pages/RecommendedPage.tsx
@@ -28,7 +28,7 @@ const RecommendedPage = () => {
 
       <main className="container mx-auto px-4 py-6 space-y-8">
         {/* Ad Banner */}
-        <AdComponent zoneId="5660536" />
+        <AdComponent zoneId="5660534" />
 
         {/* Page Title */}
         <div>
